d7f0ff4ec8ed462f0c65ce49d199e1b534debf58
tonylehuynh <tonylehuynh66@gmail.com>
2023-07-30 12:47:03 +1000

    Store date and time under one variable. Participantlist will be array

--- src/models/class.js
+++ src/models/class.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const ClassSchema = mongoose.Schema({
 	title: String,
 	date: Date,
-	time: String,
 	trainer: String,
 	description: String,
-	participantList: String
+	participantList: [String]
 })
 
 const Class = mongoose.model('Class', ClassSchema);
