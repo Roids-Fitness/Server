8ea621196eff350222c211e7bc8ca35445546819
tonylehuynh <tonylehuynh66@gmail.com>
2023-07-30 18:07:07 +1000

    Set up local mongodb connection for development

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -6,6 +9,35 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+
+let databaseURL = "";
+switch (process.env.NODE_ENV.toLowerCase()) {
+	case "production":
+		databaseURL = "mongodb://localhost:27017/roids_fitness_db";
+		break;
+	case "development":
+		databaseURL = "mongodb://localhost:27017/roids_fitness_db";
+		break;
+	case "test":
+		databaseURL = "mongodb://localhost:27017/roids_fitness_db";
+		break;
+	default:
+		console.error("Wrong environment mode. Database cannot connect.")
+};
+
+const {databaseConnector} = require("./database");
+
+databaseConnector(databaseURL)
+.then(() => {
+	console.log("Connected to database!")
+})
+.catch(error => {
+	console.log("Could not connect to database...")
+	console.log(error)
+});
+
+
 
 app.get("/", (request, response) => {
 	response.json({
