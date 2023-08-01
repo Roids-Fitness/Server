d4b8a21517f4889f2102f6a06027b50ea5e8d6a6
tonylehuynh <tonylehuynh66@gmail.com>
2023-08-01 21:07:32 +1000

    Auth middlewares added for all necessary user and classes routes

--- src/routes/classes_routes.js
+++ src/routes/classes_routes.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const classesRouter = express.Router();
 const {getClasses, createClass, getClassByID, getClassTimetable, updateClass, deleteAllClasses, deleteClass, getMyClasses, saveClassToUser} = require('../controllers/classes_controller');
-
 const {validateRequest, validateAdmin} = require('../middlewares/auth_middleware');
 
 
+// Public routes (No authentication required)
+classesRouter.get('/timetable', getClassTimetable);
+classesRouter.get('/', getClasses);
+classesRouter.get('/:id', getClassByID);
 
+// Routes that require user authentication
+classesRouter.use(validateRequest);
+classesRouter.put('/save/:id', saveClassToUser);
+classesRouter.get('/myclasses', getMyClasses);
 
-
-classesRouter.get("/timetable", getClassTimetable);
-
-classesRouter.get("/", getClasses);
-
-classesRouter.get("/:id", getClassByID);
-
-
-// Below routes require user authentication
-classesRouter.put("/save/:id", saveClassToUser);
-classesRouter.get("/myclasses", getMyClasses);
-
-// Below routes require authentication AND admin
-classesRouter.post("/", createClass);
-classesRouter.put("/:id", updateClass);
-classesRouter.delete("/deleteall", validateRequest, validateAdmin, deleteAllClasses);
-classesRouter.delete("/:id", deleteClass);
+// Routes that require both user and admin authentication
+classesRouter.use(validateAdmin);
+classesRouter.post('/', createClass);
+classesRouter.put('/:id', updateClass);
+classesRouter.delete('/deleteall', deleteAllClasses);
+classesRouter.delete('/:id', deleteClass);
 
 module.exports = classesRouter;
--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const usersRouter = express.Router();
 const {getUsers, signup, login, updateUser, deleteUser} = require('../controllers/users_controller');
+const {validateRequest, validateAdmin} = require('../middlewares/auth_middleware');
 
-
+// Public routes (No authentication required)
 usersRouter.post("/signup", signup);
 usersRouter.post("/login", login);
 
-
-// Requires authentication but NOT admin
+// Routes that require user authentication
+usersRouter.use(validateRequest);
 usersRouter.put("/:id", updateUser);
 
-
-// Below routes require auth AND admin
+// Routes that require both user and admin authentication
+usersRouter.use(validateAdmin);
 usersRouter.get("/", getUsers);
 usersRouter.delete("/:id", deleteUser);
-
 
 module.exports = usersRouter;
