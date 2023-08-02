b3fe70092c3de5aab3839c070b845f4023481669
tonylehuynh <tonylehuynh66@gmail.com>
2023-08-02 18:30:47 +1000

    order of classes routes changed for put requests

--- src/routes/classes_routes.js
+++ src/routes/classes_routes.js
@@ -13,12 +13,12 @@ classesRouter.get('/:id', getClassByID);
 
 // Routes that require user authentication
 classesRouter.use(validateRequest);
+classesRouter.put('/update/:id', validateAdmin, updateClass);
 classesRouter.put('/:id', classSignup);
 
 // Routes that require both user and admin authentication
 classesRouter.use(validateAdmin);
 classesRouter.post('/', createClass);
-classesRouter.put('/update/:id', updateClass);
 classesRouter.delete('/deleteall', deleteAllClasses);
 classesRouter.delete('/:id', deleteClass);
 
