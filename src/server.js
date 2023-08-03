6b30b31815fd81f2de11ea33af9866454d92a945
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-08-03 15:17:58 +1000

    feat: added cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,14 @@ const app = express();
 
 const PORT = process.env.PORT 
 
+const cors = require("cors");
+let corsOptions = {
+    origin: ["http://localhost:3000", "https://roids-fitness.netlify.app"],
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
