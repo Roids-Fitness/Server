e2e92e3b6e312ca272b0d36919ed7950494a973c
tonylehuynh <tonylehuynh66@gmail.com>
2023-08-06 12:25:56 +1000

    Added new front end deployment link for cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 // Configure Cross-Origin Resource Sharing (CORS) settings
 const cors = require("cors");
 let corsOptions = {
-	origin: ["http://localhost:3000", "https://roids-fitness.netlify.app"],
+	origin: ["http://localhost:3000", "https://roids-fitness.netlify.app", "https://roids-fitness.vercel.app/"],
 	optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
